e2a804c6aa0a28fe1cee2e6f47c9694920adfa72
Soulless-1 <matt.e.fleener@gmail.com>
2025-09-14 16:22:46 -0400

    Added additional lines to OptionsGump

--- src/ClassicUO.Client/Game/UI/Gumps/OptionsGump.cs
+++ src/ClassicUO.Client/Game/UI/Gumps/OptionsGump.cs
@@ -518,17 +518,17 @@ namespace ClassicUO.Game.UI.Gumps
                 )
             );
 			
-			//section.Add
-            //(
-            //    _useFastRotation = AddCheckBox
-            //    (
-            //        null,
-            //        ResGumps.UseFastRotation,
-            //        _currentProfile.UseFastRotation,
-            //        startX,
-            //        startY
-            //    )
-            //);
+			section.Add
+            (
+                _useFastRotation = AddCheckBox
+                (
+                    null,
+                    ResGumps.UseFastRotation,
+                    _currentProfile.UseFastRotation,
+                    startX,
+                    startY
+                )
+            );
 
             section.Add
             (
